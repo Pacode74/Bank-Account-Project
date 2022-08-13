e3637ad3aeea706ecede1185003e5851d9c45e8b
Pacode74 <77023283+Pacode74@users.noreply.github.com>
2022-08-13 21:08:03 +0200

    In TimeZone.py created offset_hours and offset_minutes arguments, wrote validation for them. In test_TimeZone.py tested those arguments.

--- app/TimeZone.py
+++ app/TimeZone.py
@@ -1,12 +1,17 @@
 import re
+import numbers
+from datetime import timedelta
 
 
 class TimeZone:
-    def __init__(self, name=None):
+    def __init__(self, offset_hours: numbers.Integral, offset_minutes: numbers.Integral, name: str = None):
         """:name is the name of timezone"""
-        self.validate_timezone_name(value=name, field_title='Timezone "name"', timezone_name='_name')
+        self._validate_timezone_name(value=name, field_title='Timezone "name"', property_name='_name')
+        self._offset_hours, self._offset_minutes = \
+            self._validate_offset_hours_and_offset_minutes(offset_hours, offset_minutes)
+        self._offset = offset  # offset comes from validation func
 
-    def validate_timezone_name(self,  field_title, timezone_name, value=None):
+    def _validate_timezone_name(self, field_title, property_name, value=None):
         """Refactoring Error for timezone name"""
         m = re.compile(r'[a-zA-Z]*$')
         if value is None:
@@ -14,34 +19,58 @@ class TimeZone:
         elif value == "":
             raise ValueError(f"{field_title} parameter is en empty string. Please input valid string.")
         elif not isinstance(value, str):
-            raise TypeError("Wrong input type. Input must be alphabetic string.")
+            raise TypeError(f"Wrong input type. {field_title} must be alphabetic string.")
         elif not m.match(value):
-            raise KeyError("The input must only be alphabet characters and no paces allowed.")
+            raise KeyError(f"The {field_title} parameter must only be alphabet characters and no paces allowed.")
         elif len(value) > 5:
             raise ValueError("The name cannot be more that 5 characters.")
-        setattr(self, timezone_name, value)
+        setattr(self, property_name, value)
+
+    @staticmethod
+    def _validate_offset_hours_and_offset_minutes(offset_hours, offset_minutes):
+        if not isinstance(offset_hours, numbers.Integral):
+            raise ValueError('Hour offset must be an integer.')
+
+        if not isinstance(offset_minutes, numbers.Integral):
+            raise ValueError('Minutes offset must be an integer.')
+
+        if offset_minutes < -59 or offset_minutes > 59:
+            raise ValueError('Minutes offset must between -59 and 59 (inclusive).')
+
+        # for time delta sign of minutes will be set to sign of hours
+        global offset
+        offset = timedelta(hours=offset_hours, minutes=offset_minutes)
+        # offsets are technically bounded between -12:00 and 14:00
+        # see: https://en.wikipedia.org/wiki/List_of_UTC_time_offsets
+        if offset < timedelta(hours=-12, minutes=0) or offset > timedelta(hours=14, minutes=0):
+            raise ValueError('Offset must be between -12:00 and +14:00.')
+
+        return offset_hours, offset_minutes
 
     @property
     def name(self):
         return self._name
 
-    def __repr__(self):
-        return f"TimeZone(name='{self.name}')"
+    @property
+    def offset(self):
+        return self._offset
 
-#
-# if __name__ == "__main__":
-#     t = TimeZone("MTS")
-#     # t = TimeZone("")
-#     # t = TimeZone()
-#     # t = TimeZone('TMS2')
-#     # t = TimeZone('TMS ')
-#     # t = TimeZone("ABCDEF")
-#     print(t)
-#     print(t.name)
-#     # t.name = "ZOO"
-#     # print(t.name)
-#     print(hasattr(t, "name"))
-#     print(hasattr(t, "_name"))
+    def __repr__(self):
+        return (f"TimeZone(name='{self.name}', "
+                f"offset_hours={self._offset_hours}, "
+                f"offset_minutes={self._offset_minutes})")
+
+    def __eq__(self, other):
+        """This is needed if we want to compare TimeZones()"""
+        return (isinstance(other, TimeZone) and
+                self.name == other.name and
+                self._offset_hours == other._offset_hours and
+                self._offset_minutes == other._offset_minutes)
+
+    """If I want TimeZones is usable as keys, sa we defined __eq__ method,
+    I would also have to defined __hash__ method. This class is not hashable."""
+
+
 
 
 
--- tests/test_TimeZone.py
+++ tests/test_TimeZone.py
@@ -1,12 +1,16 @@
+"""Important: For import mark 'Bank Account Project` as Source Root"""
+
+import numbers
 import unittest
 from app.TimeZone import TimeZone
+from datetime import timedelta, datetime
 from string import punctuation
 
 
 class TestNameAttribute(unittest.TestCase):
     def setUp(self):
-        self.t = TimeZone('MTS')
-        self.t1 = TimeZone('mTS')
+        self.t = TimeZone(-0, 0, 'MTS')
+        self.t1 = TimeZone(-0, 0, 'mTS')
 
     def test_name_value_easy(self):
         """Easy Level Test - test the program runs correctly
@@ -21,35 +25,39 @@ class TestNameAttribute(unittest.TestCase):
     def test_raiseError_medium(self):
         """Medium Level Testing - test the program runs correctly
            given the wrong input information."""
-        with self.assertRaises(Exception):
-            TimeZone(None)
-        self.assertRaises(Exception, TimeZone, None)
-        self.assertRaises(ValueError, TimeZone, "")
-        self.assertRaises(ValueError, TimeZone, "ABCDEFG")
-        self.assertRaises(KeyError, TimeZone, "MTS5")
-        self.assertRaises(TypeError, TimeZone, 123)
+        self.assertRaises(Exception, TimeZone, 0, 0, None)
+        self.assertRaises(ValueError, TimeZone, 0, 0, "")
+        self.assertRaises(ValueError, TimeZone, 0, 0, "ABCDEFG")
+        self.assertRaises(KeyError, TimeZone, 0, 0, "MTS5")
+        self.assertRaises(TypeError, TimeZone, 0, 0, 123)
 
     def test_input_wrong_type_raise_error_hard(self):
         """Hard Level Testing - test the program runs correctly
             given any kind of input. Input that the user unlikely to pass as arguments."""
         lst_type = [int, str, float, complex, list, tuple, range, dict, set, frozenset]
         for entry in lst_type:
-            self.assertRaises(TypeError, TimeZone, entry)
+            self.assertRaises(TypeError, TimeZone, 0, 0, entry)
 
     def test_input_special_characters_raise_error_hard(self):
         for char in set(punctuation):
-            self.assertRaises(KeyError, TimeZone, f'T{char}m')
+            self.assertRaises(KeyError, TimeZone, 0, 0, f'T{char}m')
 
     def test_error_message_on_alphabetic_characters_only_hard(self):  # (1)
-        with self.assertRaisesRegex(KeyError, "The input must only be alphabet characters and no paces allowed."):
-            # counter("MTS")
-            TimeZone('MTS ')
-            TimeZone(' MTS')
-            TimeZone('M Ts')
-            TimeZone('MTS21')
-            # looping all special characters in the test:
-            for char in set(punctuation):
-                TimeZone(f'MT{char}S')
+        self.assertRaisesRegex(KeyError, 'The Timezone "name" parameter must only be alphabet characters and no '
+                                         'paces allowed.', TimeZone, 0, 0, '1TS')
+        self.assertRaisesRegex(KeyError, 'The Timezone "name" parameter must only be alphabet characters and '
+                                         'no paces allowed.', TimeZone, 0, 0, 'MTS ')
+        self.assertRaisesRegex(KeyError, 'The Timezone "name" parameter must only be alphabet characters and '
+                                         'no paces allowed.', TimeZone, 0, 0, ' MTS')
+        self.assertRaisesRegex(KeyError, 'The Timezone "name" parameter must only be alphabet characters and '
+                                         'no paces allowed.', TimeZone, 0, 0, 'M Ts')
+        self.assertRaisesRegex(KeyError, 'The Timezone "name" parameter must only be alphabet characters and '
+                                         'no paces allowed.', TimeZone, 0, 0, 'MTS21')
+
+        # looping all special characters in the test:
+        for char in set(punctuation):
+            self.assertRaisesRegex(KeyError, 'The Timezone "name" parameter must only be alphabet characters and no'
+                                             ' paces allowed.', TimeZone, 0, 0, f'MT{char}S')
 
     # def test_input_special_characters(self):
     #     for char in set(punctuation):
@@ -80,13 +88,123 @@ class TestNameAttribute(unittest.TestCase):
         del self.t1
 
 
+class TestOffsetHourAndOffsetMinutes(unittest.TestCase):
+    def setUp(self):
+        self.t = TimeZone(-1, -30, "ABC")
+
+    def test_integral_easy(self):
+        TimeZone(-1, -30, "ABC")
+
+    def test_integer_medium(self):
+        self.assertTrue(type(self.t._offset_hours), isinstance(self.t._offset_hours, numbers.Integral))
+        self.assertTrue(type(self.t._offset_minutes), isinstance(self.t._offset_minutes, numbers.Integral))
+
+    def test_integer_easy_medium(self):
+        self.assertRaises(ValueError, TimeZone, "a", 0, "ABC")
+        self.assertRaises(ValueError, TimeZone, 0, "a", "ABC")
+        self.assertRaises(ValueError, TimeZone, "a", "b", "ABC")
+
+    def test_input_wrong_type_raise_error_hard(self):
+        """Hard Level Testing - test the program runs correctly
+            given any kind of input. Input that the user unlikely to pass as arguments."""
+        lst_type = [int, str, float, complex, list, tuple, range, dict, set, frozenset]
+        for entry in lst_type:
+            self.assertRaises(ValueError, TimeZone, entry, 0, "ABC")
+            self.assertRaises(ValueError, TimeZone, 0, entry, "ABC")
+            self.assertRaises(ValueError, TimeZone, entry, entry, "ABC")
+
+    def test_input_special_characters_raise_error_hard(self):
+        for char in set(punctuation):
+            self.assertRaises(ValueError, TimeZone, char, 0, "ABC")
+
+    def test_offset_hour_boundaries(self):
+        """Hour offsets are technically bounded between -12:00 and 14:00."""
+        with self.assertRaises(ValueError):
+            TimeZone(-13, 0, "ABC")
+            TimeZone(15, 0, "ABC")
+
+    def test_offset_minutes_boundaries(self):
+        """Minutes offset must between -59 and 59 (inclusive)."""
+        self.assertRaises(ValueError, TimeZone, 0, -60, "ABC")
+        self.assertRaises(ValueError, TimeZone, 0, 60, "ABC")
+
+    def test_offset_minutes_hours_boundaries(self):
+        """Testing both hour and minutes boundaries"""
+        with self.assertRaises(ValueError):
+            TimeZone(-12, -1, "ABC")
+            TimeZone(14, 1, "ABC")
+
+    def tearDown(self):
+        del self.t
+
+
+class TestOffsetAttribute(unittest.TestCase):
+    def setUp(self):
+        self.t = TimeZone(-1, -30, "ABC")
+
+    def test_offset_easy(self):
+        """Easy Level Test - test the program runs correctly
+        given the correct input information."""
+        self.assertEqual(self.t.offset, timedelta(hours=-1, minutes=-30))
+
+    def test_offset_plus_datetime(self):
+        """Easy Level Test - test the program runs correctly
+        given the correct input information."""
+        dt = datetime.utcnow()
+        self.assertEqual(self.t.offset + dt, timedelta(hours=-1, minutes=-30) + dt)
+
+    def test_instance_has_offset_attribute(self):
+        msg = 'The time instance does not have a category attribute.'
+        self.assertTrue(hasattr(self.t, 'offset'), msg)
+
+    def tearDown(self):
+        del self.t
+
+
+class TestEQ(unittest.TestCase):
+    """Testing Instance Equality"""
+
+    def test_timezones_equal(self):
+        tz1 = TimeZone(-1, -30, 'ABC')
+        tz2 = TimeZone(-1, -30, 'ABC')
+        self.assertEqual(tz1, tz2)
+
+    def test_timezones_not_equal_without_name_subset(self):
+        """Option 1 - Testing is one instance not equal to another instance"""
+        tz = TimeZone(-1, -30, 'ABC')
+
+        test_timezones = (
+            TimeZone(-1, -30, 'DEF'),
+            TimeZone(-1, 0, 'ABC'),
+            TimeZone(1, -30, 'ABC'),
+            # TimeZone(offset_hours=-1, offset_minutes=-30, name='ABC')  # here is an error
+        )
+        for i, test_tz in enumerate(test_timezones):
+            with self.subTest(test_number=f'Test # {i}'):
+                self.assertNotEqual(tz, test_tz)
+
+    def test_timezones_not_equal_with_name_subset(self):
+        """Option 2 - Testing is one instance not equal to another instance"""
+        tz = TimeZone(-1, -30, 'ABC')
+
+        test_timezones = (
+            ('test1', TimeZone(-1, -30, 'DEF')),
+            ('test2', TimeZone(-1, 0, 'ABC')),
+            ('test3', TimeZone(1, -30, 'ABC')),
+            # ('test4', TimeZone(offset_hours=-1, offset_minutes=-30, name='ABC')),  # here is an error
+        )
+        for i, test_tz in test_timezones:
+            with self.subTest(test_number=f'Test # {i}'):
+                self.assertNotEqual(tz, test_tz)
+
+
 class TestRepr(unittest.TestCase):
 
     def setUp(self):
-        self.t = TimeZone('MTS')
+        self.t = TimeZone(offset_hours=-1, offset_minutes=30, name="MTS")
 
     def test_repr_method(self):
-        self.assertEqual(repr(self.t), "TimeZone(name='MTS')")
+        self.assertEqual(repr(self.t), "TimeZone(name='MTS', offset_hours=-1, offset_minutes=30)")
 
     def tearDown(self):
         del self.t
@@ -117,7 +235,6 @@ if __name__ == "__main__":
 # In index.html check TimeZone.py
 
 
-
 # Notes:
 
 # (1)
@@ -137,4 +254,3 @@ if __name__ == "__main__":
 # unmatched parentheses) or when some other error occurs during
 # compilation or matching. It is never an error if a string contains
 # no match for a pattern.
-
